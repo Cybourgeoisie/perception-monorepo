ab6babbe0078015c51d92c6c35153bf248bdc9f0
cybourgeoisie <bheidorn@neondistrict.io>
2024-03-24 08:53:22 -0400

    refactor: summary bot can be achieved internally using autobot classic

--- packages/adapters/index.ts
+++ packages/adapters/index.ts
@@ -1,6 +1,5 @@
 import AutoBotAdapter from "./autobot/AutoBotAdapter";
 import ChatGPTBotAdapter from "./chatgptbot/ChatGPTBotAdapter";
 import CodeBotAdapter from "./codebot/CodeBotAdapter";
-import SummaryBotAdapter from "./summarybot/SummaryBotAdapter";
 
-export const Adapters = [AutoBotAdapter, ChatGPTBotAdapter, CodeBotAdapter, SummaryBotAdapter];
+export const Adapters = [AutoBotAdapter, ChatGPTBotAdapter, CodeBotAdapter];
